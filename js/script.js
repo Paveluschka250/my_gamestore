70ceb3a0e129ccb22f96acb826bdd2fe63bf3359
Paul Gross <p.gross6408@gmail.com>
2025-06-25 14:14:32 +0200

    feat: add comment box, description, reviews, tags and platforms to game card

--- js/script.js
+++ js/script.js
@@ -11,16 +11,82 @@ function renderGameList() {
   }
 }
 
+// function renderGameObjects() {
+//   let html = ""
+// }
+
 function renderPlatforms(platforms) {
   let html = "";
   for (let i = 0; i < platforms.length; i++) {
-    const platform = platforms[i];
-    html += /*html*/`
-      <img src="${platform.icon}" alt="${platform.name}" class="platform-icon">
-    `
+    let platform = platforms[i];
+    html += /*html*/ `
+      <img src="${platform.icon}" alt="${platform.name}">
+    `;
   }
   return html;
 }
+
+function renderTags(tags) {
+  let html = "";
+  for (let i = 0; i < tags.length; i++) {
+    let tag = tags[i];
+    html += /*html*/ `
+      <p>${tag}</p>
+    `;
+  }
+  return html;
+}
+
+function renderCommentbox(comments) {
+  let html = "";
+  for (let i = 0; i < comments.length; i++) {
+    const comment = comments[i];
+    html += /*html*/ `
+  <div class="comment">
+    <div class="comment-header">
+      <strong>${comment.name}</strong>
+    </div>
+    <div class="comment-text">
+      ${comment.comment}
+    </div>
+    <div class="comment-meta">
+      ⭐ ${comment.rating} &nbsp; | &nbsp; 🗓️ ${comment.date}
+    </div>
+  </div>
+`;
+  }
+  return html;
+}
+
+function renderRatingStars(rating) {
+  let imageFile = "";
+
+  if (rating >= 9.0 && rating < 10.0) {
+    imageFile = "10-star-rating-icon.svg";
+  } else if (rating >= 8.0 && rating < 9.0) {
+    imageFile = "9-star-rating-icon.svg";
+  } else if (rating >= 7.0 && rating < 8.0) {
+    imageFile = "8-star-rating-icon.svg";
+  } else if (rating >= 6.0 && rating < 7.0) {
+    imageFile = "7-star-rating-icon.svg";
+  } else if (rating >= 5.0 && rating < 6.0) {
+    imageFile = "6-star-rating-icon.svg";
+  } else if (rating >= 4.0 && rating < 5.0) {
+    imageFile = "5-star-rating-icon.svg";
+  } else if (rating >= 3.0 && rating < 4.0) {
+    imageFile = "4-star-rating-icon.svg";
+  } else if (rating >= 2.0 && rating < 3.0) {
+    imageFile = "3-star-rating-icon.svg";
+  } else if (rating >= 1.0 && rating < 2.0) {
+    imageFile = "2-star-rating-icon.svg";
+  } else {
+    imageFile = "1-star-rating-icon.svg";
+  }
+
+  return `<img src="assets/icons/stars/${imageFile}" alt="Rating: ${rating}" class="rating-stars">`;
+}
+
+
 
 function createGameCard(index) {
   const game = games[index];
@@ -43,6 +109,18 @@ function createGameCard(index) {
       </div>
       <div class="platforms">
         ${renderPlatforms(game.platforms)}
+      </div>
+      <div class="tags">
+        ${renderTags(game.tags)}
+      </div>
+      <div class="reviews">
+        <p>${game.rating} ${renderRatingStars(game.rating)}</p>
+      </div>
+      <div class="description">
+        <p>${game.description}</p>   
+      </div>
+      <div class="commentbox">
+        ${renderCommentbox(game.comments)}
       </div>
     </div>
   `;
